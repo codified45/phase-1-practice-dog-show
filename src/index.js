c89a41c4f68c2f61fd1d07f8f6b493cf72827daa
Jack Lach <jack.lach4@gmail.com>
2023-02-27 02:10:11 -0500

    put Edit Dog button into cell

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const tableBody = document.getElementById('table-body');
+    const dogForm = document.getElementById('dog-form');
     fetch('http://localhost:3000/dogs')
     .then(res => res.json())
         .then(obj => {
@@ -10,19 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
             let cell1 = document.createElement('td');
             let cell2 = document.createElement('td');
             let cell3 = document.createElement('td');
+            let cell4 = document.createElement('td');
             let editBtn = document.createElement('button');
             cell1.textContent = element.name;
             cell2.textContent = element.breed;
             cell3.textContent = element.sex;
             editBtn.textContent = 'Edit Dog';
+            editBtn.id = element.id;
             document.addEventListener('click', editDog);
-            tableRow.append(cell1, cell2, cell3, editBtn);
+            cell4.append(editBtn);
+            tableRow.append(cell1, cell2, cell3, cell4);
             tableBody.appendChild(tableRow);
             };
+        });
 
-
-        })
-    function editDog() {
+    function editDog(e) {
         console.log('Im in EditDog');
+        
+    };
+
+    function patchDog() {
+
     };
 })
